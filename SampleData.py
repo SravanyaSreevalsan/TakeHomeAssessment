c989c3c842ce93d43c71bd0ed2e73a8f00763728
SravanyaSreevalsan <90107606+SravanyaSreevalsan@users.noreply.github.com>
2024-07-29 18:47:57 +0100

    sampledata

---
+++ SampleData.py
@@ -0,0 +1,44 @@
+samplestock1 = {
+  "stockSymbol": "TEA",
+  "type": "Common",
+  "lastDividend": 0,
+  "fixedDivided": "",
+  "parValue":100
+}
+samplestock2 = {
+  "stockSymbol" : "POP",
+  "type" : "Common",
+  "lastDividend": 8,
+  "fixedDivided": "",
+  "parValue":100
+}
+samplestock3 = {
+  "stockSymbol": "ALE",
+  "type": "Common",
+  "lastDividend": 23,
+  "fixedDivided": "",
+  "parValue":60
+}
+
+samplestock4 = {
+  "stockSymbol": "GIN",
+  "type": "Preferred",
+  "lastDividend": 8,
+  "fixedDivided": "2%",
+  "parValue":100
+}
+samplestock5 = {
+  "stockSymbol": "JOE",
+  "type": "Common",
+  "lastDividend": 13,
+  "fixedDivided": "",
+  "parValue":250
+}
+
+samplestock = {
+  "samplestock1" : samplestock1,
+  "samplestock2" : samplestock2,
+  "samplestock3" : samplestock3,
+  "samplestock4" : samplestock4,
+  "samplestock5" : samplestock5,
+}
