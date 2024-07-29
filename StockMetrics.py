9e84e744f9d55293085622a11b11b94c7a3346b2
sravanyasreevalsan <isravanya@gmail.com>
2024-07-29 19:42:06 +0100

    StockMetrics classes and TradeRecorder files

---
+++ StockMetrics.py
@@ -0,0 +1,16 @@
+class StockMetrics:
+  def __init__(self, stockSymbol, type, lastDividend, fixedDividend, parValue):
+    self.stockSymbol = stockSymbol
+    self.type = type
+    self.lastDividend = lastDividend
+    self.fixedDividend = fixedDividend
+    self.parValue = parValue
+
+  def calculateDividendYield(self):
+    print("Result")
+
+  def calculatePbyERatio(self):
+      print("Result")
+
+
+
