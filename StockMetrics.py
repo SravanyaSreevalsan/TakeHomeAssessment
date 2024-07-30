a60fb14fe59954f23af803c3f953ee79093d1ff6
sravanyasreevalsan <isravanya@gmail.com>
2024-07-30 08:13:09 +0100

    trade recording logic

--- StockMetrics.py
+++ StockMetrics.py
@@ -1,16 +1,18 @@
 class StockMetrics:
-  def __init__(self, stockSymbol, type, lastDividend, fixedDividend, parValue):
+  def __init__(self, stockSymbol, type, lastDividend, fixedDividend, parValue, price):
     self.stockSymbol = stockSymbol
     self.type = type
     self.lastDividend = lastDividend
     self.fixedDividend = fixedDividend
     self.parValue = parValue
+    self.price = price
 
   def calculateDividendYield(self):
-    print("Result")
-
+    dividendYield = self.lastDividend/self.price
+    print("Dividend Yield for this stock is "+ dividendYield)
   def calculatePbyERatio(self):
-      print("Result")
+    pbyeRatio = self.price/self.lastDividend
+    print("P by E ratio for this stock is "+ pbyeRatio)
 
 
 
---
+++ TradeRecorder.py
@@ -0,0 +1,28 @@
+
+import datetime
+class TradeRecorder:
+    def __init__(self, quantity, price, buyorsellIndicator, timestamp, listoftrades):
+        self.quantity = quantity
+        self.price = price
+        self.buyorsellIndicator = buyorsellIndicator
+        self.timestamp = datetime.time(timestamp)
+        self.listoftrades = listoftrades
+
+        #function to record all trades
+        def recordTrades(self):
+            listoftrades = []
+            listoftrades.extend([[self.timestamp, self.price,self.quantity, self.buyorsellindicator]])
+            print(listoftrades[0])
+
+        #function to get trades in last 5 minutes
+        def tradesinlastfiveminutes(self):
+            listoftradesinlast5minutes =[]
+            timenow = datetime.datetime.now()
+            for trade in listoftrades:
+                if trade[0]-timenow==5:
+                    listoftradesinlast5minutes.append(trade)
+
+
+
+
+
